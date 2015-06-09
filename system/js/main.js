46a83f3836d688b4280e5943b90b5a88794e610a
Nishan Bajracharya <nishan_cd@ymail.com>
2015-06-09 20:31:44 +0545

    New Website Base

---
+++ system/js/main.js
@@ -0,0 +1,38 @@
+$(function() {
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > ($("#main").position().top-40)) {
+            $("header").addClass("show");
+            $("header ul li").addClass("show");
+        } else {
+            $("header").removeClass("show");
+            $("header ul li").removeClass("show");
+        }
+        if ($(window).scrollTop() > ($("#contact").position().top-40)) {
+            $("header").addClass("show2");
+            $("header ul li").addClass("show2");
+        } else {
+            $("header").removeClass("show2");
+            $("header ul li").removeClass("show2");
+        }
+        if ($(window).scrollTop() > $(window).height() / 4) {
+            $("#pre-main p").addClass("show");
+        }
+    })
+    $(".principle").appear(function() {
+        $(this).addClass("show");
+    },{accY: -300})
+
+    $("#skills .more").click(function() {
+        $(".more-skills").fadeIn();
+    })
+    $("#skills .more-skills").click(function() {
+        $(".more-skills").fadeOut();
+    })
+
+    $('a[href*=#]').click(function() {
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top - 20
+        }, 500);
+        return false;
+    });
+})
